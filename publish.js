ab90eeae4c1b859507ce044464649abfddaffcd4
Tom Renn <tomrenn@gmail.com>
2015-03-19 22:48:31 -0400

    working on publish command for markdown posts

--- publish.js
+++ publish.js
@@ -1,10 +1,14 @@
 // This script is intended to parse markdown representing posts and generate
 // html content
 // -----
-
 var marked = require('marked');
 var hljs = require('highlight.js');
 var renderer = new marked.Renderer();
+var path = require('path');
+var fs = require('fs')
+
+var outputDir = 'dist/'
+var args = process.argv.slice(2);
 
 // bugfix for stupid hljs class not getting added
 // https://github.com/chjj/marked/pull/418
@@ -21,4 +25,13 @@ marked.setOptions({
   }
 });
 
-console.log(marked('```js\n console.log("hello"); \n```'));
+args.forEach(function(filename) {
+  fs.readFile(filename, 'utf8', function(err, data) {
+    var html = marked(data);
+    var newFilename = path.basename(filename, '.md') + '.html';
+    fs.writeFile(outputDir + newFilename, html, function(err){
+      console.log(err);
+    });
+  });
+});
+
